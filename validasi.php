d49b8a74142bfac66e072638cacbe1c524d6cd18
wisdahefendi <50910666+wisdahefendi@users.noreply.github.com>
2021-10-28 21:31:51 +0700

    Add files via upload

---
+++ validasi.php
@@ -0,0 +1,109 @@
+<html>
+<head>
+<style>
+.error {color: #FF0000;}
+</style>
+</head>
+<body>
+<?php
+// define variables and set to empty values
+$no = 1;
+$namaErr = $emailErr = $genderErr = $websiteErr = "";
+$nama = $email = $gender = $comment = $website = "";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if (empty($_POST["nama"])) {
+$namaErr = "Nama harus diisi";
+}else {
+$nama = test_input($_POST["nama"]);
+}
+if (empty($_POST["email"])) {
+$emailErr = "Email harus diisi";
+}else {
+$email = test_input($_POST["email"]);
+// check if e-mail address is well-formed
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+$emailErr = "Email tidak sesuai format";
+}
+}
+if (empty($_POST["website"])) {
+$website = "";
+}else {
+$website = test_input($_POST["website"]);
+}
+if (empty($_POST["comment"])) {
+$comment = "";
+}else {
+$comment = test_input($_POST["comment"]);
+}
+if (empty($_POST["gender"])) {
+$genderErr = "Gender harus dipilih";
+}else {
+$gender = test_input($_POST["gender"]);
+}
+}
+function test_input($data) {
+$data = trim($data);
+$data = stripslashes($data);
+$data = htmlspecialchars($data);
+return $data;
+}
+?>
+<h2>Posting Komentar </h2>
+<p><span class = "error">* Harus Diisi.</span></p>
+<form method = "post" action = "<?php
+echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+<table>
+<tr>
+<td>Nama:</td>
+<td><input type = "text" name = "nama">
+<span class = "error">* <?php echo $namaErr;?></span>
+</td>
+</tr>
+<tr>
+<td>E-mail: </td>
+<td><input type = "text" name = "email">
+<span class = "error">* <?php echo $emailErr;?></span>
+</td>
+</tr>
+<tr>
+<td>Website:</td>
+<td> <input type = "text" name = "website">
+<span class = "error"><?php echo $websiteErr;?></span>
+</td>
+</tr>
+<tr>
+<td>Komentar:</td>
+<td> <textarea name = "comment" rows = "5" cols = "40"></textarea></td>
+</tr>
+<tr>
+<td>Gender:</td>
+<td>
+<input type = "radio" name = "gender" value = "L">Laki-Laki
+<input type = "radio" name = "gender" value = "P">Perempuan
+<span class = "error">* <?php echo $genderErr;?></span>
+</td>
+</tr>
+<td>
+<input type = "submit" name = "submit" value = "Submit">
+</td>
+</table>
+</form>
+<h2>Data yang anda isi:</h2>
+<table border="1" class="table table-hover">
+    <th class="table-primary"><center>NO.</font> </center></th>
+    <th class="table-primary"><center>NAMA </font> </center></th>
+    <th class="table-primary"><center>EMAIL </font></center></th>
+    <th class="table-primary"><center>WEBSITE</font> </center></th>
+    <th class="table-primary"><center>COMMENT</font> </center></th>
+    <th class="table-primary"><center>GENDER</font></center></th>
+  </tr>
+  <tr class="table-default"><font color="black">
+      <td><font size="1" color="black"><center><?php echo $no++; ?></center></td></font>
+      <td><font size="1" color="black"><center><?php echo $nama;?></center></td></font>
+      <td><font size="1" color="black"><center><?php echo $email;?></center></td></font>
+      <td><font size="1" color="black"><center><?php echo $website; ?></center></td></font>
+      <td><font size="1" color="black"><center><?php echo $comment; ?></center></td></font>
+      <td><font size="1" color="black"><center><?php echo $gender; ?></center></td></font>
+</table>
+</body>
+</html>
